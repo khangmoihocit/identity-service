4c43dad03ddaf7c25c0cf97691dbe32bacf3f758
khangmoihocit <23a1001d0196@students.hou.edu.vn>
2025-06-06 20:08:45 +0700

    first commit

---
+++ src/main/java/com/devteria/identityservice/controller/UserController.java
@@ -0,0 +1,71 @@
+package com.devteria.identityservice.controller;
+
+import com.devteria.identityservice.dto.request.ApiResponse;
+import com.devteria.identityservice.dto.request.UserCreationRequest;
+import com.devteria.identityservice.dto.request.UserUpdateRequest;
+import com.devteria.identityservice.dto.response.UserResponse;
+import com.devteria.identityservice.entity.User;
+import com.devteria.identityservice.service.UserService;
+import jakarta.validation.Valid;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+//controller sẽ tương tác trực tiếp với các class service
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class UserController {
+    UserService userService;
+
+
+    //post: tạo
+    @PostMapping
+    //api
+    ApiResponse<User> createUser(@RequestBody @Valid UserCreationRequest request) {
+        ApiResponse<User> apiResponse = new ApiResponse<>();
+        apiResponse.setResult(userService.createUser(request));
+        return apiResponse;
+    }
+
+    //get: lấy
+    @GetMapping
+    ApiResponse<List<UserResponse>> getUsers() {
+        ApiResponse<List<UserResponse>> apiResponse = new ApiResponse<>();
+        apiResponse.setResult(userService.getUsers());
+        return apiResponse;
+    }
+
+    //@PathVariable: truyền tham số
+    @GetMapping("/{userId}")
+    ApiResponse<UserResponse> getUser(@PathVariable String userId) {
+        ApiResponse<UserResponse> apiResponse = new ApiResponse<>();
+        apiResponse.setResult(userService.getUser(userId));
+        return apiResponse;
+    }
+
+    //put: update thong tin
+    @PutMapping("/{userId}")
+    ApiResponse<UserResponse> updateUser(@PathVariable String userId, @RequestBody UserUpdateRequest request) {
+        ApiResponse<UserResponse> apiResponse = new ApiResponse<>();
+        apiResponse.setResult(userService.updateUser(userId, request));
+        return apiResponse;
+    }
+
+    //delete: xoa
+    @DeleteMapping("/{userId}")
+    ApiResponse<String> deleteUser(@PathVariable String userId) {
+        ApiResponse<String> apiResponse = new ApiResponse<>();
+        userService.deleteUser(userId);
+        apiResponse.setMessage("User has been deleted");
+        return apiResponse;
+    }
+
+
+}
+
---
+++ src/main/java/com/devteria/identityservice/service/UserService.java
@@ -0,0 +1,67 @@
+package com.devteria.identityservice.service;
+
+import com.devteria.identityservice.dto.request.UserCreationRequest;
+import com.devteria.identityservice.dto.request.UserUpdateRequest;
+import com.devteria.identityservice.dto.response.UserResponse;
+import com.devteria.identityservice.entity.User;
+import com.devteria.identityservice.exception.AppException;
+import com.devteria.identityservice.exception.ErrorCode;
+import com.devteria.identityservice.mapper.UserMapper;
+import com.devteria.identityservice.repository.UserRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor //cac thuoc tinh final se tu tao constructor nen khong can annotation @Autowired
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true) //makeFinal khoi tao cac thuoc tinh la final
+public class UserService {
+    UserRepository userRepository;
+    UserMapper userMapper;
+
+
+    public User createUser(UserCreationRequest request){
+
+        //phuong thuc existsByUsername : giup kiem tra xem ten da ton tai chua
+        if (userRepository.existsByUsername(request.getUsername())) throw new AppException(ErrorCode.USER_EXISTED);
+
+        User user = userMapper.toUser(request);
+
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10); //10 la do phuc tap ma hoa
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+
+        //hàm trong interface UserR epository kế thừa Jpa
+        return userRepository.save(user);
+    }
+
+
+    //tra ve userResponse thay vi user
+    public List<UserResponse> getUsers(){
+        //hàm trong interface UserRepository kế thừa Jpa
+        return userMapper.toUsersResponse(userRepository.findAll());
+    }
+
+    public UserResponse getUser(String userId){
+        return userMapper.toUserResponse(userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found")));
+    }
+
+    //update user
+    public UserResponse updateUser(String userId, UserUpdateRequest request){
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        userMapper.updateUser(user, request);
+
+        return userMapper.toUserResponse(userRepository.save(user));
+    }
+
+    public void deleteUser(String idUser){
+        userRepository.deleteById(idUser);
+    }
+}
