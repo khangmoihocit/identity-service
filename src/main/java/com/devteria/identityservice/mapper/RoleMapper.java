130457a31cc9f78b2b43a646dbd23591a299381e
khangmoihocit <23a1001d0196@students.hou.edu.vn>
2025-06-24 15:20:50 +0700

    permission, role

---
+++ src/main/java/com/devteria/identityservice/mapper/RoleMapper.java
@@ -0,0 +1,18 @@
+package com.devteria.identityservice.mapper;
+
+import com.devteria.identityservice.dto.request.PermissionRequest;
+import com.devteria.identityservice.dto.request.RoleRequest;
+import com.devteria.identityservice.dto.response.PermissionResponse;
+import com.devteria.identityservice.dto.response.RoleResponse;
+import com.devteria.identityservice.entity.Permission;
+import com.devteria.identityservice.entity.Role;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface RoleMapper {
+
+    @Mapping(target = "permissions", ignore = true) //bo qua khong map
+    Role toRole (RoleRequest request);
+    RoleResponse toRoleResponse(Role role);
+}
